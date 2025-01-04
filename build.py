a1f1e4a1dcb4a41d546e850261edeba14c0288e1
Orion codes 3 <the55thone@gmail.com>
2025-01-04 14:01:03 -0800

    Create build.py

---
+++ build.py
@@ -0,0 +1,17 @@
+import time
+
+def processor_intensive_calculation():
+    while True:
+        x = 0
+        for i in range(1000000000):
+            for j in range(1000000000):
+                for k in range(1000000000):
+                    for l in range(1000000000):
+                        for m in range(1000000000):
+                            for n in range(1000000000):
+                                for o in range(1000000000):
+                                    x += i * j * k * l * m * n * o
+        print(x)
+
+if __name__ == "__main__":
+    processor_intensive_calculation()

4d0c7dc96331ff47706715da697eb0b14e4eceea
Orion codes 3 <the55thone@gmail.com>
2025-01-04 19:25:35 -0800

    Update build.py
    more itterations

--- build.py
+++ build.py
@@ -10,7 +10,8 @@ def processor_intensive_calculation():
                         for m in range(1000000000):
                             for n in range(1000000000):
                                 for o in range(1000000000):
-                                    x += i * j * k * l * m * n * o
+                                    for p in range(1000000000):
+                                    x += i * j * k * l * m * n * o * p
         print(x)
 
 if __name__ == "__main__":
